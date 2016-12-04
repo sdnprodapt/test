c10e460b9fd79c33d02f8ae2f73fc20dd1713b42
Aditya Sharma <adsharma@contractor.ciena.com>
2016-12-04 13:35:15 +0530

    add scripts to create session profile, network function and n3vpn + changes in tosca after testing

---
+++ scripts/create-l3vpn.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+INSTANCE_NAME=123
+
+source dockerdev.sh
+
+PRODUCT_ID=$(jcurl ${MARKET_URL}/products?q=resourceTypeId:juniper.resourceTypes.L3VPN | jparse [\"items\"][0][\"id\"])
+DEVICE_RID=$(jcurl ${MARKET_URL}/resources?resourceTypeId=juniper.resourceTypes.NetworkFunction\&label=jun_89 | jparse [\"items\"][0][\"id\"])
+
+cat <<EOF > l3vpn_123.json
+{
+"productId": "${PRODUCT_ID}",
+"label": "${INSTANCE_NAME}",
+"discovered": false,
+"properties":{
+            "vpn-id": ${INSTANCE_NAME},
+            "device": "${DEVICE_RID}",
+            "tpe": "some_interface",
+            "service":{
+                "svc-bandwidth": 10000
+            },
+
+            "extension": {
+                "interface": {
+                    "name": "ge-0/0/1",
+                    "l3vpnVlan": 500,
+                    "description": "NAPC-ML3-6-1-MUMBAI-20480K-NATIONAL-PAYMENTS",
+                    "bandwidth": 20480,
+                    "vlan-id": 500,
+                    "inputServicePolicy": "in_STANDARD_20480",
+                    "outputServicePolicy": "out_STANDARD_20480",
+                    "address": "10.100.0.1/30"
+                },
+                "VrfPolicy": {
+                    "device": "${DEVICE_RID}",
+                    "community": [{
+                        "community_name": "NATIONAL-PAYMENT-6-H-export-COMM",
+                        "members": [
+                            "target:9730:17053"
+                        ]
+                    }, {
+                        "community_name": "NATIONAL-PAYMENT-6-H-import-COMM",
+                        "members": [
+                            "target:9730:17053"
+                        ]
+                    }, {
+                        "community_name": "NATIONAL-PAYMENT-6-H-import-COMM1",
+                        "members": [
+                            "target:9730:17054"
+                        ]
+                    }],
+                    "policy-statement": [{
+                        "name": "NATIONAL-PAYMENT-6-H-export",
+                        "terms": [{
+                            "name": "11",
+                            "from": {
+                                "protocol": [
+                                    "direct",
+                                    "static",
+                                    "bgp"
+                                ]
+                            },
+                            "then": {
+                                "community": [{
+                                    "community_name": "NATIONAL-PAYMENT-6-H-export-COMM",
+                                    "operation": "add"
+                                }],
+                                "next-hop": {
+                                    "self": true
+                                },
+                                "accept": true
+                            }
+                        }, {
+                            "name": "last",
+                            "then": {
+                                "accept": true
+                            }
+                        }]
+                    }, {
+                        "name": "NATIONAL-PAYMENT-6-H-import",
+                        "terms": [{
+                            "name": "10",
+                            "from": {
+                                "protocol": [
+                                    "direct",
+                                    "static",
+                                    "bgp"
+                                ],
+                                "community": [
+                                    "NATIONAL-PAYMENT-6-H-import-COMM",
+                                    "NATIONAL-PAYMENT-6-H-import-COMM1"
+                                ]
+                            },
+                            "then": {
+                                "next-hop": {
+                                    "self": true
+                                },
+                                "accept": true
+                            }
+                        }, {
+                            "name": "last",
+                            "then": {
+                                "reject": true
+                            }
+                        }]
+                    }]
+                }
+            },
+            "attachment": {
+                "connection": {
+                    "routing-instance": {
+                        "device": "${DEVICE_RID}",
+                        "name": "NATIONAL-PAYMENT-6-H",
+                        "instance-type": "vrf",
+                        "interface": ["ge-0/0/1.500"],
+                        "route-distinguisher": "9730:20160912",
+                        "vrf-import": ["NATIONAL-PAYMENT-6-H-import"],
+                        "vrf-export": ["NATIONAL-PAYMENT-6-H-export"],
+                        "vrf-table-label": "",
+                        "routing-options": {
+                            "auto_export": true,
+                            "maximum-prefixes": {
+                                "limit": 5000,
+                                "threshold": 80
+                            }
+                        },
+                        "protocols_bgp": {
+                            "groups": [{
+                                "name": "NATIONAL-PAYMENT-6-H",
+                                "type": "external",
+                                "as-override": true,
+                                "peer-as": 100,
+                                "local-address": "",
+                                "local_as": 100,
+                                "neighbors": [{
+                                    "address": "10.100.0.2",
+                                    "local-address": "10.100.0.1",
+                                    "peer-as": 65001,
+                                    "as-override": true
+                                }],
+                                "export_policies": ["STATIC", "CONNECTED"]
+                            }]
+                        }
+                    },
+                    "class-of-service": {
+                        "device": "${DEVICE_RID}",
+                        "routing-instance": [{
+                            "name": "NATIONAL-PAYMENT-6-H",
+                            "classifier-name": "BACKBONE_QOS_Classifiers"
+                        }]
+                    }
+                }
+            }
+        }
+}
+EOF
+
+jcurl -d @l3vpn_123.json ${MARKET_URL}/resources | jpp
+
+rm -f l3vpn_123.json
---
+++ scripts/create-network_function-89.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DEVICE_IP="10.60.10.89"
+
+source dockerdev.sh
+
+PRODUCT_ID=$(jcurl ${MARKET_URL}/products?q=resourceTypeId:juniper.resourceTypes.NetworkFunction | jparse [\"items\"][0][\"id\"])
+SESSION_PROFILE_ID=$(jcurl $MARKET_URL/resources?resourceTypeId=juniper.resourceTypes.SessionProfile\&q=label:jun_89 | jparse [\"items\"][0][\"id\"])
+echo "${SESSION_PROFILE_ID}"
+cat <<EOF > network_function.json
+{
+"productId": "${PRODUCT_ID}",
+"label": "device_89",
+"discovered": false,
+"properties": {
+    "sessionProfile": "${SESSION_PROFILE_ID}",
+    "ipAddress": "${DEVICE_IP}"
+}
+}
+EOF
+
+jcurl -d @network_function.json ${MARKET_URL}/resources | jpp
+
+rm -f network_function.json
